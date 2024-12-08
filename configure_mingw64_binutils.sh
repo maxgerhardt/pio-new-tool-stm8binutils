f2e34322cf72bb9ff831f1f65e16ad17e71209aa
Maximilian Gerhardt <maximilian.gerhardt@rub.de>
2024-12-08 15:17:48 +0100

    Retry with prefix

--- configure_mingw64_binutils.sh
+++ configure_mingw64_binutils.sh
@@ -5,5 +5,8 @@ if [ x$ =x"" ]; then
 else
   _HOST=$HOST
 fi
+if [ x$PREFIX != x"" ]; then
+  _PREFIX="-prefix=$PREFIX"
+fi
 ../binutils-2.30/configure --target=stm8-none-elf32 --program-prefix=stm8- --with-system-readline \
---with-libexpat-prefix=/usr/$_HOST/sys-root/mingw/ --with-python=no LDFLAGS="-static" CFLAGS="-DNCURSES_STATIC" CXXFLAGS="-DNCURSES_STATIC"
+--with-libexpat-prefix=/usr/$_HOST/sys-root/mingw/ $_PREFIX --with-python=no LDFLAGS="-static" CFLAGS="-DNCURSES_STATIC" CXXFLAGS="-DNCURSES_STATIC"
