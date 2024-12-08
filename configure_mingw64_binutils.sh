79752b192d9e1205d55bb9cf827f17ef7270ca74
Maximilian Gerhardt <maximilian.gerhardt@rub.de>
2024-12-08 13:38:42 +0100

    Retry

--- configure_mingw64_binutils.sh
+++ configure_mingw64_binutils.sh
@@ -1,9 +1,9 @@
 mkdir -p mingw64_binutils
 cd mingw64_binutils
-if [ x$HOST=x"" ]; then
+if [ x$ =x"" ]; then
   _HOST=$(./../binutils-2.30/config.guess)
 else
   _HOST=$HOST
 fi
-../binutils-2.30/configure --host=$_HOST --build=$_HOST --target=stm8-none-elf32 --program-prefix=stm8- --with-system-readline \
+../binutils-2.30/configure  --target=stm8-none-elf32 --program-prefix=stm8- --with-system-readline \
 --with-libexpat-prefix=/usr/$_HOST/sys-root/mingw/ LDFLAGS=-static
